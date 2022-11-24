a56bfd761cb7bf71ee4ec464831734e6ca00aef7
Multyjog <simon.kotov.web@gmail.com>
2022-11-24 20:29:26 +0300

    - Some endpoint setup - Interfaces template created

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { createClient } from 'redis';
 import { v4 as uuidv4 } from 'uuid'
 import * as bodyParser from 'body-parser'
 
+
 dotenv.config()
 
 const app = express()
@@ -13,10 +14,12 @@ const client = createClient({
     database: parseInt(process.env.REDIS_DB_NUMBER || "0")
 })
 
+// MAIN
 app.get("/", async (req, res) => {
     res.send("Listening...")
 })
 
+// CREATE USER
 app.post('/users', async (req,res) => {
     if (!client.isReady) await client.connect();
     const user = {
@@ -24,11 +27,43 @@ app.post('/users', async (req,res) => {
         age: req.body.age || 0
     }
     const id = uuidv4()
-    await client.SET(id, JSON.stringify(user))
+    await client.set(id, JSON.stringify(user))
     res.json({ id, ...user })
 
 })
 
+// GET USER
+app.get("/users/:id", async (req, res) => {
+    if (!client.isReady) await client.connect();
+    const userId = req.params.id
+    const user = JSON.parse(await client.get(userId))
+    res.json({ ...user, id: userId })
+})
+
+// EDIT USER
+app.put("/users/:id", async (req,res) => {
+    if (!client.isReady) await client.connect();
+    const userId = req.params.id
+    const user = {
+        name: req.body.name || 'User',
+        age: req.body.age || 0
+    }
+    await client.set(userId, JSON.stringify(user))
+    res.json({ ...user, id: userId })
+})
+
+// DELETE USER
+// app.delete("users/:id", async (req,res) => {
+//     if (!client.isReady) await client.connect();
+//     const userId = req.params.id
+//     const keys = await client.keys("id")
+//     console.log(keys)
+//     const userToDel = keys.find(el => el == userId)
+//     await client.del(userToDel)
+//     res.json({ userToDel })
+// })
+
+// GET USERS
 app.get("/users", async (req, res) => {
     if (!client.isReady) await client.connect();
     const keys = await client.keys("*")
---
+++ src/models/User.ts
@@ -0,0 +1,8 @@
+interface UserData {
+    name: string
+    age: number
+}
+
+interface User extends UserData {
+    id: string
+}
